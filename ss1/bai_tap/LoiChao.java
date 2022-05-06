50afdd8c5e29dd821819bb3dde5baf695d774067
binnguyen1297 <huythao1297@gmail.com>
2022-05-06 08:56:38 +0700

    bai tap

---
+++ ss1/bai_tap/LoiChao.java
@@ -0,0 +1,12 @@
+package ss1.bai_tap;
+
+import java.util.Scanner;
+
+public class LoiChao {
+    public static void main(String[] args) {
+        System.out.println("Enter your name");
+        Scanner scanner = new Scanner(System.in);
+        String name = scanner.nextLine();
+        System.out.println("hello"+ name);
+    }
+}
