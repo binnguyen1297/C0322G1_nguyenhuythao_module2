c4e487809fd6975dde4003472a8aa15a04e3f8be
binnguyen1297 <huythao1297@gmail.com>
2022-05-09 08:05:59 +0700

    bai tap "up bai"

---
+++ src/ss3_bai_tap/DemKiTuTrongChuoi.java
@@ -0,0 +1,24 @@
+package ss3_bai_tap;
+
+import java.util.Scanner;
+
+public class DemKiTuTrongChuoi {
+//    public static void main(String[] args) {
+//        String str = "nhap vao chuoi";
+//
+//        Scanner sc = new Scanner(System.in);
+//
+//        System.out.print("nhap ki tu can tim : ");
+//        char main = sc.nextLine().charAt(0);
+//
+//        int count=0;
+//
+//        for(int i=0; i< str.length(); i++){
+//            if(main == str.charAt(i)){
+//                count++;
+//            }
+//        }
+//        System.out.print("in ra so ki tu la: "+ count);
+//    }
+
+}
---
+++ src/ss3_bai_tap/gopMang.java
@@ -0,0 +1,38 @@
+package ss3_bai_tap;
+
+import java.util.Scanner;
+
+public class gopMang {
+    public static void main(String[] args) {
+        Scanner x = new Scanner(System.in);
+        int[] arr1 = new int[5];
+        int[] arr2 = new int[5];
+
+        for(int i =0 ; i< arr1.length; i++){
+            System.out.print("Enter arr1 element "+ i + ": ");
+            arr1[i] = x.nextInt();
+        }
+        for(int i =0 ; i< arr2.length; i++){
+            System.out.print("Enter arr2 element "+ i + ": ");
+            arr2[i] = x.nextInt();
+        }
+
+        int[] arr3 = new int[arr1.length + arr2.length];
+        int j = 0;
+        for (int i = 0; i < arr3.length; i++) {
+            if (i < arr1.length) {
+                arr3[i] = arr1[i];
+            } else {
+                if (j<arr2.length) {
+                    arr3[i] = arr2[j];
+                    j++;
+                }
+            }
+        }
+        System.out.println("My array 3");
+        for (int a : arr3) {
+            System.out.print(a);
+        }
+
+    }
+}
